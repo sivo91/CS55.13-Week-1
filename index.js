cfc8cbaecc36193415b51621db23fffcbdf48292
Peter <>
2022-08-19 00:34:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  
+  res.writeHead(200, {'content-type': 'text/html'})
+  res.write('<h1 style="color:blue" >Hello Week 1</h1>')
+  res.write('<h3> Assignment 2: Git + GitHub Basics</h3>')
+  res.end()
+  
+})
+
+server.listen(5000)
